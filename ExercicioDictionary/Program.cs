3b90b79b642196ff84869bf11605923d87274042
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-11 23:32:42 -0300

    Exercicio Proposto Dictionary: Contagem de votos a partir de um arquivo externo.

---
+++ ExercicioDictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace ExercicioDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Cursos\CSharp\ExercicioDictionary\ExercicioDictionary\Files\input.txt";
+            Dictionary<string, int> votos = new Dictionary<string, int>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int voto = int.Parse(line[1]);
+
+                        if(votos.ContainsKey(name)){
+                            votos[name] += voto;
+                        }
+                        else{
+                            votos[name] = voto;
+                        }
+                    }
+                }
+                foreach (KeyValuePair<string, int> v in votos)
+                {
+                    Console.WriteLine($"{v.Key}: {v.Value}");
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Ocorreu uma falha ao tentar utilizar o arquivo");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
